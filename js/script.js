3e0b1bb689b76104a5d98bd3f6aeb00046f86a8f
joeKody <samonguro.a@gmail.com>
2022-10-28 19:36:15 +0700

    add game chooser functionality partially

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ function select(str){
 }
 
 function select_small_img(num) {
+    index = Number(num) + 1;
     return select('.small-img .img-preview:nth-child(' + num + ')');
 }
 
@@ -18,10 +19,24 @@ function change_big_img(new_img) {
     setTimeout(()=>{big_img.style.opacity = '100%'}, 350);
 }
 
-
 setInterval(() => {
     if (index > 12){index = 1;}
     // if (index < 1){index = 12;}
     change_big_img(select_small_img(String(index)));
     index++;
 }, 7270);
+
+var choose_game_btn = select('#choose-game-btn');
+choose_game_btn.addEventListener("click", ()=>{
+    var genre_inputs = document.getElementsByClassName("genre-input");
+    console.log(genre_inputs);
+    let genre_results = {};
+    for (var x = 0; x < genre_inputs.length; x++){
+        var current_item = genre_inputs.item(x);
+        if (current_item.checked){
+            genre_results[current_item.name] = current_item.value;
+        }
+    }
+    console.log(genre_results);
+    console.log(genre_results.keys);
+});
